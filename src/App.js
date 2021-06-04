458a446972000a889fbd69cac26b2031f8d8a3db
brian_andela <brian_andela@medable.com>
2021-06-04 12:57:41 +0300

    interpolation and plurality in swahili

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useTranslation, Trans } from 'react-i18next';
 
 const lngs = {
   en: { nativeName: 'English' },
-  swa: { nativeName: 'Kiswahili ' },
+  sw: { nativeName: 'Kiswahili ' },
 };
 
 function App() {
   const { t, i18n } = useTranslation();
+  const [count, setCounter] = useState(1);
   return (
     <div className='App'>
       <header className='App-header'>
@@ -19,12 +21,18 @@ function App() {
               key={lng}
               style={{ fontWeight: i18n.language === lng ? 'bold' : 'normal' }}
               type='submit'
-              onClick={() => i18n.changeLanguage(lng)}
+              onClick={() => {
+                i18n.changeLanguage(lng)
+                setCounter(count + 1)
+              }}
             >
               {lngs[lng].nativeName}
             </button>
           ))}
         </div>
+        <p>
+        <i>{t('counter', { count })}</i>
+        </p>
         <p>
           <Trans i18nKey='description.part1'>
             Edit <code>src/App.js</code> and save to reload.
--- src/i18n.js
+++ src/i18n.js
@@ -19,14 +19,19 @@ i18n
             part1: 'Edit <1>src/App.js</1> and save to reload.',
             part2: 'Learn React',
           },
+          counter: 'Changed language just once',
+          
+          counter_plural: 'Changed language already {{count}} times'
         },
       },
-      swa: {
+      sw: {
         translation: {
           description: {
             part1: 'Hariri <1>src/App.js</1> na uhifadhi ili upakie tena.',
             part2: 'Jifunze React',
           },
+          counter: 'Lugha ilibadilishwa mara moja tu',
+          counter_plural: 'Lugha imebadilika tayari mara {{count}}'
         },
       },
     },
