ce30cd0122d4ae3359d35db3be0e12024d42f00a
brian_andela <brian_andela@medable.com>
2021-06-04 12:13:43 +0300

    initial commit, swahili initial changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  swa: { nativeName: 'Kiswahili ' },
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button
+              key={lng}
+              style={{ fontWeight: i18n.language === lng ? 'bold' : 'normal' }}
+              type='submit'
+              onClick={() => i18n.changeLanguage(lng)}
+            >
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey='description.part1'>
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          {t('description.part2')}
         </a>
       </header>
     </div>
