3a99b569831da3d0e1a00f547fbc8c93a1f5acc0
HawiCaesar <brianhawi92@gmail.com>
2021-06-08 15:19:45 +0300

    even more changes

--- src/i18n.js
+++ src/i18n.js
@@ -26,7 +26,7 @@ i18n
             part1: 'Edit <1>src/App.js</1> and save to reload.',
             part2: 'Learn React',
           },
-          counter: 'Changed languagesssddsrfefre just once', 
+          counter: 'Changed languagesssddsrfefredewdew just once', 
           counter_plural: 'Changed language already {{count}} times',
           footer: {
             date: 'Today is {{date, DATE_HUGE}}'
